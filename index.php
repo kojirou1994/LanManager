cf7c1b1c2012bd1a429851d5d1d923471dd39e92
kojirou <kojirouhtc@gmail.com>
2016-04-27 22:22:55 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Index</title>
+    <style type="text/css">
+        a:link {color: #FF0000}
+        a:visited {color: #00FF00}
+        a:hover {color: #FF00FF}
+        a:active {color: #0000FF}
+    </style>
+</head>
+<body>
+<div>
+    <a href="http://<?php echo $_SERVER["SERVER_ADDR"];?>:7600" target="_blank">File Server</a><br>
+    <a href="http://<?php echo $_SERVER["SERVER_ADDR"];?>:7601" target="_blank">Aria2 Manager</a>
+</div>
+</body>
+</html>
